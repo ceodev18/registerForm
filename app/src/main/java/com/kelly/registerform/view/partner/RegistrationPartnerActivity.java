5efefe46f9661727a10e9a507c18d7a2cc10df56
ITALO <espinoza.c@pucp.pe>
2018-02-22 19:47:10 -0500

    set

--- app/src/main/java/com/kelly/registerform/view/partner/RegistrationPartnerActivity.java
+++ app/src/main/java/com/kelly/registerform/view/partner/RegistrationPartnerActivity.java
@@ -11,6 +11,7 @@ import android.provider.Settings;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -20,6 +21,13 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.kelly.registerform.BuildConfig;
 import com.kelly.registerform.R;
 import com.kelly.registerform.dataAccess.ProvinceDA;
 import com.kelly.registerform.model.ubigeo.Departamento;
@@ -27,7 +35,11 @@ import com.kelly.registerform.model.ubigeo.Provincia;
 import com.orm.query.Condition;
 import com.orm.query.Select;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class RegistrationPartnerActivity extends AppCompatActivity {
@@ -35,13 +47,14 @@ public class RegistrationPartnerActivity extends AppCompatActivity {
     private static final int PICK_IMAGE = 100;
     private Button b_next,b_dni,b_photo;
     private Context context;
-    private Spinner gender_spinner,regions_spinner,province_spinner;
+    private Spinner gender_spinner,regions_spinner,province_spinner,district_spinner;
     private String lattitude,longitude;
     private TextView tv_dni,tv_photo;
     private LocationManager locationManager;
     private Uri imageUri;
     private ImageView foto_gallery;
-    private ArrayList<String>arrayListDeparment;
+    private ArrayList<String>arrayListDeparment,nameProvince,nameDistrict;
+    private ArrayList<Integer>idProvince,idDistrict;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,9 +78,14 @@ public class RegistrationPartnerActivity extends AppCompatActivity {
         //gender_spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arraySpinner));
         regions_spinner=(Spinner)findViewById(R.id.regions_spinner);
         province_spinner=findViewById(R.id.province_spinner);
+        district_spinner=findViewById(R.id.district_spinner);
         ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(this,
                 android.R.layout.simple_spinner_dropdown_item,
                 arrayListDeparment);
+        nameDistrict=new ArrayList<>();
+        nameProvince=new ArrayList<>();
+        idDistrict=new ArrayList<>();
+        idProvince=new ArrayList<>();
         regions_spinner.setAdapter(spinnerArrayAdapter);
 
     }
@@ -113,6 +131,7 @@ public class RegistrationPartnerActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 String data =  regions_spinner.getSelectedItem().toString();
+                fillDistrict(idProvince.get(i));
 
             }
 
@@ -163,17 +182,98 @@ public class RegistrationPartnerActivity extends AppCompatActivity {
 
     }
     private void fillProvinceList(Departamento departamento){
-        ProvinceDA provinceDA = new ProvinceDA();
-        ArrayList<Provincia>list=provinceDA.getDepartamentos(departamento.getId_departamento());
-        System.out.println(list.size()  );
-        ArrayList<String> nameList=new ArrayList<>();
-        for (Provincia provincia : list) {
-            nameList.add(provincia.getName());
-            System.out.println(provincia.getName());
-        }
-        ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(this,
-                android.R.layout.simple_spinner_dropdown_item,
-                nameList);
-        province_spinner.setAdapter(spinnerArrayAdapter);
+
+        int id=departamento.getId_departamento();
+
+        RequestQueue queue = Volley.newRequestQueue(this.context);
+
+        String url = BuildConfig.BASE_URL+"lista_ubigeo.php?tipo=PROV&idubigeosuperior="+id+"&token=lpsk.21568$lsjANPIO02";
+        System.out.println(url);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObj = new JSONObject(response);
+                            JSONObject jsonObj2 = (JSONObject) jsonObj.get("departamentos");
+                            Iterator<?> keys = jsonObj2.keys();
+                            idProvince=new ArrayList<>();
+                            nameProvince=new ArrayList<>();
+                            while( keys.hasNext() ) {
+                                String key = (String)keys.next();
+                                System.out.println(key);
+                                if ( jsonObj2.get(key) instanceof JSONObject ) {
+                                    JSONObject jsonDepartment = (JSONObject) jsonObj2.get(key);
+                                    int  idDis = (int)jsonDepartment.get("id");
+                                    String name =(String)jsonDepartment.get("ubigeo");
+                                    idProvince.add(idDis);
+                                    nameProvince.add(name);
+                                }
+                            }
+                            ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(context,
+                                    android.R.layout.simple_spinner_dropdown_item,
+                                    nameProvince);
+                            province_spinner.setAdapter(spinnerArrayAdapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            province_spinner.setAdapter(null);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Fail","That didn't work!");
+
+            }
+        });
+        queue.add(stringRequest);
+
+    }
+
+    private void fillDistrict(int idProvince){
+        RequestQueue queue = Volley.newRequestQueue(this.context);
+        String url = BuildConfig.BASE_URL+"lista_ubigeo.php?tipo=DIST&idubigeosuperior="+idProvince+"&token=lpsk.21568$lsjANPIO02";
+        System.out.println(url);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObj = new JSONObject(response);
+                            JSONObject jsonObj2 = (JSONObject) jsonObj.get("departamentos");
+                            Iterator<?> keys = jsonObj2.keys();
+                            idDistrict=new ArrayList<>();
+                            nameDistrict=new ArrayList<>();
+                            while( keys.hasNext() ) {
+                                String key = (String)keys.next();
+                                System.out.println(key);
+                                if ( jsonObj2.get(key) instanceof JSONObject ) {
+                                    JSONObject jsonDepartment = (JSONObject) jsonObj2.get(key);
+                                    int  idDis = (int)jsonDepartment.get("id");
+                                    String name =(String)jsonDepartment.get("ubigeo");
+                                    idDistrict.add(idDis);
+                                    nameDistrict.add(name);
+                                }
+                            }
+                            ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(context,
+                                    android.R.layout.simple_spinner_dropdown_item,
+                                    nameDistrict);
+                            district_spinner.setAdapter(spinnerArrayAdapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            district_spinner.setAdapter(null);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Fail","That didn't work!");
+
+            }
+        });
+        queue.add(stringRequest);
+
     }
 }
